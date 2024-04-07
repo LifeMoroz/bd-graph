df7455e3e7509c94fba640f211b978c58552ecf9
Ruslan Galimov <r.galimov@bitmaker.fi>
2024-04-07 22:59:40 +0000

    ! add time range ! use unified symbol

--- main.py
+++ main.py
@@ -1,39 +1,61 @@
 import datetime
-
-import ccxt
 import logging
+from collections import defaultdict
+
 import matplotlib.pyplot as plt
+
+from utils import get_data
 
 logger = logging.getLogger(__name__)
 
-SYMBOL = "QRDO-USDT"
-EXCHANGES = ["kucoin", "gateio", "mexc", "bitmart", "hitbtc", "bitfinex", "bitget"]
+RESOLUTION = "1m"
 
 
-symbol_variances = [SYMBOL, SYMBOL.replace("-", "_"), SYMBOL.replace("-", "")]
+def plot(symbol: str, data: dict):
+    plt.figure(figsize=(14, 7))
+    for exchange, candles in data.items():
+        plt.plot(
+            [datetime.datetime.utcfromtimestamp(c[0] / 1000) for c in candles], [c[-2] for c in candles], label=exchange
+        )
 
-data = {}
-for exchange_id in EXCHANGES:
-    exchange = getattr(ccxt, exchange_id)()
-    for variance in symbol_variances:
-        try:
-            data[exchange_id] = exchange.fetch_ohlcv(variance, timeframe="1m", limit=100)
-            break
-        except Exception as e:
-            pass
-    else:
-        logger.error("Can't fetch %s ohlcv for %s", SYMBOL, exchange_id)
+    plt.title(f"{symbol} Price Chart")
+    plt.legend()
+    plt.show()
 
 
-start_point = max(o[0][0] for o in data.values())
-data = {k: list(filter(lambda o: o[0] >= start_point, candles)) for k, candles in data.items()}
+def run(symbol, exchanges, start: datetime.datetime, end: datetime.datetime):
+    data = defaultdict(list)
+    while start and end and start < end:
+        _end = min(start + datetime.timedelta(minutes=100), end)
+        logger.info("Fetching data for [%s, %s)", start, _end)
+        _data = get_data(
+            "fetch_ohlcv",
+            symbol,
+            exchanges,
+            timeframe=RESOLUTION,
+            limit=100,
+            since=int(start.timestamp() * 1000),
+            params={"until": int(_end.timestamp() * 1000)},
+        )
+        for k, v in _data.items():
+            if v:
+                data[k] += v
+        start = _end
+    start_point = max(o[0][0] for o in data.values())
+    end_point = end.timestamp() * 1000
+    data = {
+        k: list(sorted(filter(lambda o: end_point >= o[0] >= start_point, candles), key=lambda o: o[0]))
+        for k, candles in data.items()
+    }
+    plot(symbol, data)
 
-plt.figure(figsize=(14, 7))
-for exchange, candles in data.items():
-    plt.plot(
-        [datetime.datetime.utcfromtimestamp(c[0] / 1000) for c in candles], [c[-2] for c in candles], label=exchange
-    )
 
-plt.title(f"{SYMBOL} Price Chart")
-plt.legend()
-plt.show()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    SYMBOL = "QRDO-USDT"
+    EXCHANGES = ["kucoin", "gateio", "mexc", "bitmart", "hitbtc", "bitfinex", "bitget"]
+    # [start, end)
+    START = datetime.datetime(2024, 4, 2, 0, 0)
+    END = datetime.datetime(2024, 4, 2, 4, 0)
+
+    run(SYMBOL, EXCHANGES, START, END)
--- utils.py
+++ utils.py
@@ -6,20 +6,12 @@ import ccxt
 logger = logging.getLogger(__name__)
 
 
-def symbol_variances(symbol: str):
-    return [symbol, symbol.replace("-", "_"), symbol.replace("-", "")]
-
-
-def get_data(method: str, symbol: str, exchanges: list[str]):
+def get_data(method: str, symbol: str, exchanges: list[str], **kwargs):
     data = {}
     for exchange_id in exchanges:
         exchange = getattr(ccxt, exchange_id)()
-        for variance in symbol_variances(symbol):
-            try:
-                data[exchange_id] = getattr(exchange, method)(variance)
-                break
-            except Exception:
-                pass
-        else:
-            logger.error("Can't fetch %s ohlcv for %s", symbol, exchange_id)
+        try:
+            data[exchange_id] = getattr(exchange, method)(symbol.replace("-", "/"), **kwargs)
+        except Exception as e:
+            logger.warning("Failed to get data: %s", e)
     return data
